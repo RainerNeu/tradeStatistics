aa980b8da48a3bc5d3815d7dd45169ff8636d36b
Rainer Neuberger <neuberger.rainer@gmail.com>
2020-03-30 14:21:17 +0200

    minor change in comment

--- return.jl
+++ return.jl
@@ -1,8 +1,8 @@
 """
     getNominalReturn
 calculates the return of a "profit and loss"-series as absolute value
-per definition: the net profit or loss of an investment defined by the change in value
-over a stated period including all costs and incomes
+per definition: the net profit or loss of an investment defined by 
+the change in value over a stated period including all costs and incomes
 Formula is:     return = (endValue - initialValue)/initialValue
 """
 function getNominalReturn(arr::Array{Float64,1})::Float64
