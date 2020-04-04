2e18e2e355d597a129e86fdad1fa0a4cb5f7ceef
Rainer Neuberger <neuberger.rainer@gmail.com>
2020-04-04 23:43:51 +0200

    update of Maximum Drawdown
    now works if only one value (can be negative) is in the trade series

--- maxDD.jl
+++ maxDD.jl
@@ -1,16 +1,24 @@
 """
-    getMaxDDfromProfitAndLoss(arr)
+    getMaxDD(arr)
 calculates the maximum Drawdown of profit and loss series
 (not the aggregated profit and loss series)
 be aware: data must be sorted by date upfront!
 """
 function getMaxDD(arr::Array{Float64,1})::Float64
-    maxDD=0.0                                                       # initialize maxDD with Type FLOAT
-    aggregatedSeries = getProfitAndLossAggregatedSeries(arr);       # create aggregatedSeries
-    for n in eachindex(arr)                                         # iterate through trades
-        max::Float64 = maximum(aggregatedSeries[1:n])               # Calculate max of equity-curve
-        if (max-aggregatedSeries[n])>maxDD                          # if difference of actual equity curve to max greater
-            maxDD = (max-aggregatedSeries[n])                       # this will be new maxDD
+    peak=0.0
+    maxDD=0.0                                                                   # initialize maxDD with Type FLOAT
+    equity = 0.0
+    aggregatedSeries = getAccumulatedProfitLossSeries(arr);                     # create aggregatedSeries
+
+    for n in eachindex(aggregatedSeries)
+        equity = aggregatedSeries[n]
+        #Check if new peak is available
+        if equity>peak
+            peak = equity
+        else
+            if abs(peak-equity) > maxDD
+                maxDD = abs(peak - equity)
+            end
         end
     end
     return maxDD
